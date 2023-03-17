28afa009b6352ce7b5880f4fba6a9d1168db39db
Bastien Loisel <bloisel@c2r9p1.42nice.fr>
2023-03-17 18:19:59 +0100

    d

--- include/pipex.h
+++ include/pipex.h
@@ -6,7 +6,7 @@
 /*   By: bloisel <bloisel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 11:09:27 by bloisel           #+#    #+#             */
-/*   Updated: 2023/02/25 16:00:45 by bloisel          ###   ########.fr       */
+/*   Updated: 2023/03/17 13:15:36 by bloisel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,16 @@
 # include "fcntl.h"
 # include "stdlib.h"
 # include "unistd.h"
+# include <sys/types.h>
+# include <sys/uio.h>
 
 typedef struct s_data
 {
+	char	*envh;
+	char	**path;
 	int		error;
+	int		infile;
+	int		outfile;
 }	t_data;
 
 void    print_tab(char **tab);
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: bloisel <bloisel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 11:11:23 by bloisel           #+#    #+#             */
-/*   Updated: 2023/02/25 17:49:36 by bloisel          ###   ########.fr       */
+/*   Updated: 2023/03/17 16:51:35 by bloisel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,45 @@ void	init_data(t_data *dta)
 	error = 0;
 }
 
+char	*look_road(char **env,t_data *dta)
+{
+	while (ft_strncmp("PATH", *env, 4))
+	{
+		env++;
+	}
+	dta->envh = *env + 5;
+	return(*env + 5);
+}
+
+void	path(char *env, t_data *dta, char **envb)
+{
+	int i;
+	int j;
+
+	j = 0;
+	char	*cmd;
+
+	i = -1;
+	dta->path = ft_split(dta->envh, ':');
+	ft_printf("%s\n",dta->envh);
+	while (dta->path[++i])
+	{
+		cmd = ft_strjoin(dta->path, "/ls");
+		access(cmd, F_OK);
+		//execve(cmd, dta->path, envb);
+		//perror("Error");
+		// j++;
+		free(cmd);
+	}	
+}
+
 int main(int argc, char **argv, char **env)
 {
-	print_tab(env);
-	
+	t_data dta;
+	char **envb;
+	envb = env;
+	// usr/bin (le + de cmd)
+	printf("%s\n",look_road(env, &dta));
+	path(dta.envh, &dta, envb);
 	return (0);
 }
-		
